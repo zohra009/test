621746931f4f2083d0936e7eb78156cbbfcb8d0f
zor009 <anzohra@outlook.com>
2023-10-18 15:42:21 -0400

    for loop added

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("connected with external file");
+
+var a = 1;
+var b = 2;
+
+if(a+b>4){
+    console.log(a+b);
+} 
+else if(a+b <1)
+{
+    console.log(a+b);
+}
+else
+console.log ("hi");
+
+for(var num=0;num <50;num=num+2)
+    console.log(num);
