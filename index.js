29f25b10bcbfa8cd25d9cc8e66f7a6e8e568c461
zor009 <anzohra@outlook.com>
2023-10-18 15:59:43 -0400

    added for in and for of loop

--- index.js
+++ index.js
@@ -17,3 +17,13 @@ for(var num=0;num <50;num=num+2)
     console.log(num);
 var a= [1,2,3,4,5,6,7,8,9]
 a.forEach(item => console.log(item));
+
+//this gave us the index of all the items in the array
+for(var item in a){
+    console.log(item);
+}
+
+// this outputs the items in the array
+for(var item of a){
+    console.log(item);
+}
